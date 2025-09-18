c366085b71b33fd7c2be752e6eb925cf1ffe9b26
xrpalmen186 <xrpalmen186@ieshnosmachado.org>
2025-09-18 09:47:47 +0200

    tarea, pide dos valores y muestra la suma. documentación: Entrada y salida estándar.

---
+++ tarea1_18_9_25.py
@@ -0,0 +1,7 @@
+a = input("Introduce el primer valor: ")
+b = input("Introduce el segundo valor: ")
+resultado = int(a) + int(b)
+
+print("El primer valor es:", a)
+print("El segundo valor es:", b)
+print("El resultado de la suma es:", resultado)
