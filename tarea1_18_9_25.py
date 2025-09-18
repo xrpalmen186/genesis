06fbecb8be02e8a10011491313a0bf93bd86b08d
xrpalmen186 <xrpalmen186@ieshnosmachado.org>
2025-09-18 10:45:22 +0200

    añadimos apuntes

--- tarea1_18_9_25.py
+++ tarea1_18_9_25.py
@@ -14,3 +14,17 @@ print("El resultado de la suma es:", suma)
 print("El resultado de la resta es:", resta)
 print("El resultado de la multiplicacion es:", multi)
 print("El resultado de la division es:", divi)
+
+# print("La suma de los dos números es %d" %(a+b))
+
+# print("Suma: %d, Resta: %d" %(a+b,a-b))
+
+'''
+Con %d decimos que ese lugar va un decimal, y lo que hay fuera con otro %
+es lo que se sustituirá en %d como decimal.
+'''
+
+# La manera que nosotros usaremos será la siguiente:
+
+print(f"Suma: {a+b}, Resta: {a-b}")
+# Esto interpreta lo que hay entre llaves y meterlo en la misma cadena.
