800ea9f05aaa27eda61e44a11189d7578cf5ab69
xrpalmen186 <xrpalmen186@ieshnosmachado.org>
2025-09-18 10:10:06 +0200

    añadimos resta, multiplicacion y división

--- tarea1_18_9_25.py
+++ tarea1_18_9_25.py
@@ -1,7 +1,14 @@
-a = input("Introduce el primer valor: ")
-b = input("Introduce el segundo valor: ")
-resultado = int(a) + int(b)
+a = int(input("Introduce el primer valor: "))
+b = int(input("Introduce el segundo valor: "))
+suma = a + b
+resta = a - b
+multi = a * b
+divi = a / b
 
 print("El primer valor es:", a)
 print("El segundo valor es:", b)
-print("El resultado de la suma es:", resultado)
+print("######################################")
+print("El resultado de la suma es:", suma)
+print("El resultado de la resta es:", resta)
+print("El resultado de la multiplicacion es:", multi)
+print("El resultado de la division es:", divi)
