a74c7519349a86e675202992da9793039bfb16a7
rayelus5 <rayelusanom828@gmail.com>
2025-09-20 16:13:06 +0200

    enunciados de ejercicios de listas y cadenas

---
+++ ejercicios/ud1/18-09-25/ejercicioscadenas.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# 1. Crear un programa que lea por teclado una cadena y un carácter, e inserte el carácter entre cada letra de la cadena. Ej: separar y , debería devolver s,e,p,a,r,a,r
+
+
+# 2. Crear un programa que lea por teclado una cadena y un carácter, y reemplace todos los dígitos en la cadena por el carácter. Ej: su clave es: 1540 y X debería devolver su clave es: XXXX
+
+
+# 3. Crea un programa python que lea una cadena de caracteres y muestre la siguiente información:
+'''
+- La primera letra de cada palabra. Por ejemplo, si recibe Universal Serial Bus debe devolver USB.
+
+- Dicha cadena con la primera letra de cada palabra en mayúsculas. Por ejemplo, si recibe república argentina debe devolver República Argentina.
+
+- Las palabras que comiencen con la letra A. Por ejemplo, si recibe Antes de ayer debe devolver Antes ayer.
+'''
+
+
+# 4. Escribir funciones que dadas dos cadenas de caracteres:
+'''
+- Indique si la segunda cadena es una subcadena de la primera. Por ejemplo, cadena es una subcadena de subcadena.
+
+- Devuelva la que sea anterior en orden alfabético. Por ejemplo, si recibe kde y gnome debe devolver gnome.
+'''
+
+
+# 5. Escribir un programa python que dado una palabra diga si es un palíndromo. Un palídromo Un palíndromo es una palabra, número o frase que se lee igual hacia adelante que hacia atrás. Ejemplo: reconocer.
---
+++ ejercicios/ud1/18-09-25/ejercicioslistas.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# 1. Lee por teclado números y guardalo en una lista, el proceso finaliza cuando metamos un número negativo. Muestra el máximo de los números guardado en la lista, muestra los números pares.
+
+
+# 2. Realizar un programa que, dada una lista, devuelva una nueva lista cuyo contenido sea igual a la original pero invertida. Así, dada la lista [‘Di’, ‘buen’, ‘día’, ‘a’, ‘papa’], deberá devolver [‘papa’, ‘a’, ‘día’, ‘buen’, ‘Di’].
+
+
+# 3. Dada una lista de cadenas, pide una cadenena por teclado e indica si está en la lista, indica cuantas veces aparece en la lista, lee otra cadena y sustituye la primera por la segunda en la lista, y por último borra la cadena de la lista
+
+
+# 4. Dado una lista, hacer un programa que indique si está ordenada o no.
