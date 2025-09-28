1a6d131de8ef45f3c2261c7511e6f6f2eca5dd6f
rayelus5 <rayelusanom828@gmail.com>
2025-09-28 20:34:21 +0200

    primer ejercicio propuesto de listas resuelto

--- pruebas/listas/practica.py
+++ pruebas/listas/practica.py
@@ -12,9 +12,58 @@ En otra lista, debemos guardar el número de veces que aparece cada palabra clav
     - keywords = ["ordenador", "portátil"]
     - ocurrencias = [5, 7]
 
+Pista: Podemos pasar de una cadena de texto a una lista de palabras mediante el método texto.split().
 '''
 
+texto=""""Seguramente hayas notado que tu productividad ha bajado desde que trabajas desde casa.
 
+Es muy importante que logremos teletrabajar efectivamente y de manera autorregulada.
+Esto se traduce en finalizar antes nuestras tareas y evitar jornadas laborales interminables.
+El primer consejo y uno de los más importantes ya te lo he dado en el apartado anterior.
+
+Tenemos que construir un espacio de trabajo en el que nos sintamos cómodos y dispongamos de todas las herramientas necesarias para teletrabajar. En la medida de lo posible, es importante teletrabajar siempre en el mismo lugar.
+De esta forma, nuestro cerebro asocia el sitio con la acción de trabajar y nos hará estar más focalizados en nuestras tareas."""
+
+keywords = []
+ocurrencias = []
+
+for x in range(5):
+   keyword = input("Introduce una palabra clave o fin para terminar: ")
+   if keyword == "fin":
+      break
+   else:
+      keywords.append(keyword)
+
+print(keywords)
+
+posicion = 0
+while(True):
+   if posicion >= len(keywords):
+      break
+   if keywords[posicion] == '':
+      keywords.pop(posicion)
+   elif keywords[posicion].isnumeric():
+      keywords.pop(posicion)
+   else:
+      posicion += 1
+
+print("Lista de keywords corregida")
+print(keywords)
+
+texto = texto.replace('.', '').replace(',', '').split()
+# Quitamos los caracteres especiales como , . y lo convertimos en lista
+
+for x in range(len(keywords)):
+   ocurrencias.append(0)
+
+for palabra in texto:
+   for keyword in keywords:
+      if keyword == palabra:
+         pos = keywords.index(keyword)
+         ocurrencias[pos] += 1
+         break
+      
+print(ocurrencias)
 
 '''
 
